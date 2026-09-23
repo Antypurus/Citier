9c987a198be4c7a08cdd7c8b7e3edfa8a4250c3e
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2026-09-23 01:21:54 +0100

    placeholder file read, need to understand further

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn main() -> std::io::Result<()> {
+    let log = fs::read_to_string("WoWCombatLog-090726_205649.txt")?;
+    println!("Hello, world! {}", log);
+    Ok(())
 }
